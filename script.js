45ac46b3d4392f6e6bd8e7ddeb0fdbc4e870cc61
SophieeV <sophievesin@wanadoo.fr>
2021-04-13 10:18:16 +0200

    ajout fichiers

---
+++ script.js
@@ -0,0 +1,5 @@
+let movies = ["Star Wars","Le Seigneur des Anneaux","Sacré Graal","Premier Contact"],p = "";
+for(let i=0; i < movies.length; i++){
+    p+= (i+1) + " : " + movies[i]+ "\n";
+}
+alert(p);
